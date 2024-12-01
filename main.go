eb99d003e8a594b256556aa4999d918c53fee53c
MaximBel01 <maxim.bel01@gmail.com>
2024-12-01 16:19:59 +0300

    Can create users now

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/MaximBel01/rss_try/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 
@@ -17,6 +25,20 @@ func main() {
 	portstring := os.Getenv("PORT")
 	if portstring == "" {
 		log.Fatal("Port not found in .env")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if portstring == "" {
+		log.Fatal("DB_URL not found in .env")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Cant connect to database: ", err)
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
 	}
 
 	router := chi.NewRouter()
@@ -32,6 +54,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -41,7 +64,7 @@ func main() {
 	}
 	log.Printf("Server start on port %v", portstring)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
